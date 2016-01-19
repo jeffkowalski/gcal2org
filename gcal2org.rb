9c848acbd1c5a1697f485aa532e6b6b91854af48
Jeff Kowalski <jeff.kowalski@gmail.com>
2016-01-18 17:04:07 -0800

    Add client and client_secrets headers, for refresh

--- gcal2org.rb
+++ gcal2org.rb
@@ -5,6 +5,8 @@
 #                      https://console.developers.google.com
 # Google API Ruby Client:  https://github.com/google/google-api-ruby-client
 
+require 'google/api_client'
+require 'google/api_client/client_secrets'
 require 'google/api_client/auth/installed_app'
 require 'google/api_client/auth/storage'
 require 'google/api_client/auth/storages/file_store'
